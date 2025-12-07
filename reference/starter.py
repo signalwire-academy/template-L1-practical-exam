3fda5bb9c7b1442f44dc82edd55dea7c21fad75d
Brian West <brian@signalwire.com>
2025-12-07 06:51:41 -0800

    Add starter template

---
+++ reference/starter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Level 1 Practical Exam - Starter Template
+
+Build a complete voice agent with:
+- Proper greeting prompt
+- At least 2 SWAIG functions
+- Voice and language configuration
+"""
+
+from signalwire_agents import AgentBase
+
+# TODO: Create your agent
+agent = AgentBase(name="my-agent")
+
+# TODO: Add prompt sections
+
+# TODO: Configure voice
+
+# TODO: Add SWAIG functions
+
+
+if __name__ == "__main__":
+    agent.run()
